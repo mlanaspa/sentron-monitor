f7a35e79f3484434096e15cc002156e7de5e4285
mlanaspa <miglanaspa@gmail.com>
2025-12-14 22:29:52 +0100

    fix: Update TypeScript and Vite configurations
    Adjusts `tsconfig.json` and `vite.config.ts` to use `moduleResolution: "node"` instead of `"bundler"`. This change is necessary for proper module resolution, particularly when working with Node.js environments and common dependencies.
    
    Also removes explicit `types: ["vite/client"]` and `types: ["node"]` as these are often implicitly handled by Vite and Node.js environments, simplifying the configuration. The `NodeJS.ProcessEnv` declaration is removed from `vite-env.d.ts` as `loadEnv` directly exposes these variables.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+/// <reference types="node" />
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Carga variables locales (.env)
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
